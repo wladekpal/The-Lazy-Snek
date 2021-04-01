52d59b20c90db0b23cbb6316182e641101132de8
kzkaktus <thekzkaktus@gmail.com>
2021-04-01 22:06:45 +0200

    basic blocks classes + wall

--- tests/unit/test_blocks.py
+++ tests/unit/test_blocks.py
@@ -3,39 +3,9 @@ from src.engine.direction import Direction
 import pytest
 import pygame
 
-@pytest.fixture
-def wall():
-    return Wall()
-
-
-@pytest.fixture
-def direction_north():
-    return Direction('N')
-
-
-@pytest.fixture
-def direction_south():
-    return Direction('S')
-
-
-@pytest.fixture
-def direction_east():
-    return Direction('E')
-
-
-@pytest.fixture
-def direction_west():
-    return Direction('W')
-
-
-@pytest.fixture
-def snake_to_interact_with_wall():
-    return 'snake'
-
-
 def test_wall_and_snake_cannot_interact():
     try:
-        wall().interact_with_snake(snake_to_interact_with_wall())
+        Wall().interact_with_snake('snake')
         assert False
     except WallInteractionError:
         assert True
@@ -43,7 +13,7 @@ def test_wall_and_snake_cannot_interact():
 
 def test_wall_cannot_be_moved_north():
     try:
-        wall().move(direction_north())
+        Wall().move(Direction('N'))
         assert False
     except WallInteractionError:
         assert True
@@ -51,7 +21,7 @@ def test_wall_cannot_be_moved_north():
 
 def test_wall_cannot_be_moved_south():
     try:
-        wall().move(direction_south())
+        Wall().move(Direction('S'))
         assert False
     except WallInteractionError:
         assert True
@@ -59,7 +29,7 @@ def test_wall_cannot_be_moved_south():
 
 def test_wall_cannot_be_moved_east():
     try:
-        wall().move(direction_east())
+        Wall().move(Direction('E'))
         assert False
     except WallInteractionError:
         assert True
@@ -67,38 +37,33 @@ def test_wall_cannot_be_moved_east():
 
 def test_wall_cannot_be_moved_west():
     try:
-        wall().move(direction_west())
+        Wall().move(Direction('W'))
         assert False
     except WallInteractionError:
         assert True
 
 
 def test_wall_denies_moving_north():
-    wall = Wall()
-    assert not wall.check_move(direction_north())
+    assert not Wall().check_move(Direction('N'))
 
 
 def test_wall_denies_moving_south():
-    wall = Wall()
-    assert not wall.check_move(direction_south())
+    assert not Wall().check_move(Direction('S'))
 
 
 def test_wall_denies_moving_east():
-    wall = Wall()
-    assert not wall.check_move(direction_east())
+    assert not Wall().check_move(Direction('E'))
 
 
 def test_wall_denies_moving_west():
-    wall = Wall()
-    assert not wall.check_move(direction_west())
+    assert not Wall().check_move(Direction('W'))
 
 
 def test_snake_cant_move_towards_wall_when_next_to_it():
-    wall = Wall()
-    assert not wall.check_snake_move(direction_east())
-    assert not wall.check_snake_move(direction_west())
-    assert not wall.check_snake_move(direction_north())
-    assert not wall.check_snake_move(direction_south())
+    assert not Wall().check_snake_move(Direction('E'))
+    assert not Wall().check_snake_move(Direction('W'))
+    assert not Wall().check_snake_move(Direction('N'))
+    assert not Wall().check_snake_move(Direction('S'))
 
 
 def test_block_class_object_cant_be_created():
