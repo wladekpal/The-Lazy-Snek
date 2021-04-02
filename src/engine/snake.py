9f5c5f385b2fe3fd566aebbc5405c90c7d7230f1
wladekpal <wladek.palucki@gmail.com>
2021-04-02 20:03:29 +0200

    added drawing method

--- src/engine/snake.py
+++ src/engine/snake.py
@@ -2,6 +2,8 @@ from src.engine.direction import Direction
 from src.engine.field import Field
 from src.engine.board import Board
 
+import pygame
+
 
 class Snake:
     is_alive = True
@@ -12,9 +14,31 @@ class Snake:
         self.color = color
         self.direction = direction
         self.board = board
+        self.head_texture = pygame.image.load('../assets/snek-head-' + color + '.png')
+        self.body_texture = pygame.image.load('../assets/snek-body-' + color + '.png')
+        self.tail_texture = pygame.image.load('../assets/snek-tail-' + color + '.png')
 
-    def self_draw(self, frame, x, y):
-        pass
+    def self_draw(self, frame):
+        if not self.is_alive:
+            pass
+        
+        board_height = frame.get_height()
+        board_width = frame.get_width()
+
+        max_field_height = board_height // len(self.board.fields)
+        max_field_width = board_width // len(self.board.fields[0])
+
+        field_side = min(max_field_height, max_field_width)
+
+        field_area_height = field_side * len(self.board.fields)
+        field_are_width = field_side * len(self.board[0].fields)
+
+        frame.blit(self.head_texture, (field_area_width + field_side * self.segments[0][0], field_area_height + field_side * self.segments[0][1]))
+
+        for body_segment in segments[1:1]:
+            frame.blit(self.body_texture, (field_area_width + field_side * body_segment[0], field_area_height + field_side * body_segment[1]))
+
+        frame.blit(self.tail_texture, (field_area_width + field_side * self.segments[-1][0], field_area_height + field_side * self.segments[-1][1]))
 
     def move(self):
         current_head_coords = self.segments[-1]
