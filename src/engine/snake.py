fa0fe3f29c2689ad9848591eef1884d5df9ec596
wladekpal <wladek.palucki@gmail.com>
2021-04-02 19:07:04 +0200

    Improvements to snake class

--- src/engine/snake.py
+++ src/engine/snake.py
@@ -1,6 +1,6 @@
-from direction import Direction
-from field import Field
-from board import Board
+from src.engine.direction import Direction
+# from src.engine.field import Field
+# from src.engine.board import Board
 
 
 class Snake:
@@ -24,11 +24,17 @@ class Snake:
         if not new_field.check_snake_move(self, self.direction):
             self.destroy()
 
-        new_field.snake_entered(self, self.direction)
-
-        self.segments.append(new_head_coords)
         if not self.grow_at_next_move:
             self.segments.pop(0)
+        else:
+            self.grow_at_next_move = False
+
+        new_field.snake_entered(self, self.direction)
+
+        if not self.is_alive:
+            return
+
+        self.segments.append(new_head_coords)
 
     def grow(self):
         self.grow_at_next_move = True
@@ -38,11 +44,12 @@ class Snake:
             field = self.board.request_field(segment)
             field.remove_snake()
         self.is_alive = False
+        self.board.snake_died(self)
 
     def interact_with_snake(self, other_snake):
         snake_head_coords = self.segments[-1]
-        other_head_coords = other_snake.segment[-1]
-        if snake_head_coords == other_head_coords
+        other_head_coords = other_snake.segments[-1]
+        if snake_head_coords == other_head_coords:
             self.destroy()
         other_snake.destroy()
         
