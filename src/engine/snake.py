65d95afcfd57336fc520276331a76ec2e5a8b55c
wladekpal <wladek.palucki@gmail.com>
2021-04-03 20:00:41 +0200

    Updated drawing method, small changes in __init_

--- src/engine/snake.py
+++ src/engine/snake.py
@@ -16,37 +16,19 @@ class Snake:
         self.body_texture = pygame.image.load(body_path)
         self.tail_texture = pygame.image.load(tail_path)
 
-    def self_draw(self, frame):
+        for segment in self.segments:
+            field_to_place = self.board.request_field(segment)
+            field_to_place.place_snake(self)
+
+    def draw_segment(self, frame, segment_coords, draw_coords):
         if not self.is_alive:
             pass
-
-        board_height = frame.get_height()
-        board_width = frame.get_width()
-
-        max_field_height = board_height // len(self.board.fields)
-        max_field_width = board_width // len(self.board.fields[0])
-
-        field_side = min(max_field_height, max_field_width)
-
-        field_area_height = field_side * len(self.board.fields)
-        field_area_width = field_side * len(self.board[0].fields)
-
-        start_x = (board_width - field_area_width) // 2
-        start_y = (board_height - field_area_height) // 2
-
-        resized_head_texture = pygame.transform.scale(self.head_texture, (field_side, field_side))
-        resized_body_texture = pygame.transform.scale(self.body_texture, (field_side, field_side))
-        resized_tail_texture = pygame.transform.scale(self.tail_texture, (field_side, field_side))
-
-        disp_coords = (start_x + field_side * self.segments[0][0], start_y + field_side * self.segments[0][1])
-        frame.blit(resized_head_texture, disp_coords)
-
-        for body_segment in self.segments[1:1]:
-            disp_coords = (start_x + field_side * body_segment[0], start_y + field_side * body_segment[1])
-            frame.blit(resized_body_texture, disp_coords)
-
-        disp_coords = (start_x + field_side * self.segments[-1][0], start_y + field_side * self.segments[-1][1])
-        frame.blit(resized_tail_texture, disp_coords)
+        if self.segments[-1] == segment_coords:
+            frame.blit(self.head_texture, draw_coords)
+        elif self.segments[0] == segment_coords:
+            frame.blit(self.tail_texture, draw_coords)
+        else:
+            frame.blit(self.body_texture, draw_coords)
 
     def move(self):
         current_head_coords = self.segments[-1]
@@ -58,6 +40,8 @@ class Snake:
 
         if not self.grow_at_next_move:
             self.segments.pop(0)
+            field_to_remove = self.board.request_field(self.segments[0])
+            field_to_remove.remove_snake()
         else:
             self.grow_at_next_move = False
 
