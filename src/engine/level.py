2bb311fc99a724a0b849f8a2e2ea2cf13c8c7196
Kamil <kamil.jonak18@gmail.com>
2021-04-03 20:03:06 +0200

    Update level

--- src/engine/level.py
+++ src/engine/level.py
@@ -2,30 +2,38 @@ import json
 from src.engine.blocks import Block, Wall
 from src.engine.field import Field
 
+
 class Level:
     def __init__(self, file_name):
-        self.file_name = file_name
+        with open(file_name) as json_file:
+            self.level_description = json.load(json_file)
 
-        self.level_name = None
-        self.level_creator = None
-        self.block_placement = None
+        self.level_name = self.level_description["level_name"]
+        self.level_creator = self.level_description["level_creator"]
+        self.block_placement = self.level_description["block_placement"]
+        self.snake_data = self.level_description["snake_data"]
+        self.available_blocks = self.level_description["available_blocks"]
+
+        self.snake_pointer = 0
         self.board = None
         self.snakes = None
-        self.available_blocks = None
-        self.snake_pointer = 0
+
+        self.convert_board()
+        self.convert_snakes()
 
     def reload_level(self):
-        with open(self.file_name) as json_file:
-            level_description = json.load(json_file)
+        self.level_name = self.level_description["level_name"]
+        self.level_creator = self.level_description["level_creator"]
+        self.block_placement = self.level_description["block_placement"]
+        self.snake_data = self.level_description["snake_data"]
+        self.available_blocks = self.level_description["available_blocks"]
 
-        self.level_name = level_description["level_name"]
-        self.level_creator = level_description["level_creator"]
-        self.block_placement = level_description["block_placement"]
-        self.snakes = level_description["snake_data"]
-        self.available_blocks = level_description["available_blocks"]
         self.snake_pointer = 0
         self.board = None
+        self.snakes = None
+
         self.convert_board()
+        self.convert_snakes()
 
     def convert_board(self):
         board = []
@@ -44,8 +52,26 @@ class Level:
 
         self.board = Board(board)
 
-    def tick(self):
-        pass
+    def convert_snakes(self):
+        self.snakes = []
+        for snake_d in self.snake_data:
+            snake = Snake(
+                snake_d["placement"],
+                snake_d["color"],
+                snake_d["direction"],
+                self.board
+            )
+            self.snakes.append(snake)
+
+    def tick(self) -> bool:
+        self.snakes[self.snake_pointer].move()
+        self.snake_pointer += 1
+        self.snake_pointer %= len(self.snakes)
+
+        for snake in self.snakes:
+            if not snake.is_alive:
+                return False
+        return True
 
     def self_draw(self, frame, x, y, side_length):
         self.board.self_draw(frame, x, y, side_length)
