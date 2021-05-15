aebcbee209fb3659c936caae2e3eb679adf778de
wladekpal <wladek.palucki@gmail.com>
2021-05-15 17:16:15 +0200

    added file explorer to save a given file

---
+++ src/display/file_explorer.py
@@ -0,0 +1,11 @@
+from tkinter import filedialog
+import tkinter
+
+def select_destination(level_to_save):
+    root = tkinter.Tk()
+    root.withdraw()
+    path_to_file = filedialog.asksaveasfilename(filetypes=[('JSON file', '*.json')], defaultextension='*.json')
+
+    if path_to_file:
+         created_file = open(path_to_file, 'w')                
+         created_file.write(level_to_save)
